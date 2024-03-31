1d4ef5f214551499639d5b88b13e9894d2583543
Mortis <oskar.sammet@gmail.com>
2024-03-31 18:06:07 +0200

    save

--- main.c
+++ main.c
@@ -1,27 +1,21 @@
 #include <gtk/gtk.h>
 
-#include "main.h"
 #include "window.h"
 
 int main(int argc, char* argv[]) {
-    if (argc <= 1) {
-        runWindow(argc, argv);
-    } else {
-        runWithFile("", argc, argv);
-    }
-
-    return 0;
-}
-
-void runWindow(int argc, char* argv[]) {
     Window window;
     window.title = "HDR Image Viewer";
     window.application = gtk_application_new("de.pp.image", G_APPLICATION_DEFAULT_FLAGS);
     window.width = 1080;
     window.height = 620;
 
-    initialize_window(&window, argc, &argv);
-}
+    if (argc <= 1) {
+        window.title = "HDR Image Viewer";
+        initialize_window(&window, argc, &argv);
+    } else {
+        window.title = argv[1];
+        initialize_window(&window, argc, &argv);
+    }
 
-void runWithFile(char* fileName, int argc, char* argv[]) {
+    return 0;
 }
--- main.h
+++ main.h
@@ -1,8 +1,5 @@
 #ifndef PROGRAMMINGPROJECT_MAIN_H
 #define PROGRAMMINGPROJECT_MAIN_H
 
-void runWindow(int argc, char* argv[]);
-void runWithFile(char* fileName, int argc, char* argv[]);
-
 #endif
 
